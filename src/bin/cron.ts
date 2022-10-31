63796c8b2a1bb03a2fa44e3c30d79528009d506c
Dhyego Calota <dhyegofernando@gmail.com>
2022-10-30 22:52:50 -0400

    feat: Support debug mode

--- src/bin/cron.ts
+++ src/bin/cron.ts
@@ -3,20 +3,29 @@
 import buildConfigFromEnv from '@/build-config-from-env';
 import buildDefaultLogger from '@/build-default-logger';
 import WorkflowRegistry from '@/workflow-registry';
+import { Env } from '@/types/env';
 
 const config = buildConfigFromEnv();
 const logger = buildDefaultLogger();
 const workflows = WorkflowRegistry.getOrInitialize().buildWorkflows({ config, logger });
 
-workflows.forEach((workflow) => {
-  const cron = workflow.getOrBuildCronExecution();
-  cron.start();
-});
+if (config.env === Env.DEVELOPMENT) {
+  workflows.forEach((workflow) => {
+    workflow.execute();
+  });
+}
 
-process.on('SIGTERM', () => {
-  logger.info('SIGTERM signal received');
+if (config.env === Env.PRODUCTION) {
   workflows.forEach((workflow) => {
     const cron = workflow.getOrBuildCronExecution();
-    cron.stop();
+    cron.start();
   });
-});
+
+  process.on('SIGTERM', () => {
+    logger.info('SIGTERM signal received');
+    workflows.forEach((workflow) => {
+      const cron = workflow.getOrBuildCronExecution();
+      cron.stop();
+    });
+  });
+}
