9c7e5a812ab392fcb10919c68b7ec3b0ed9131ac
Dhyego Calota <dhyegofernando@gmail.com>
2022-10-30 21:35:07 -0400

    fix: Support individual and group applications

--- src/workflows/commands/get-available-appointment-consulates/index.ts
+++ src/workflows/commands/get-available-appointment-consulates/index.ts
@@ -1,6 +1,4 @@
 export { default as BuildAppointmentConsulatesWorkflowCommand } from './build-appointment-consulates';
 export { default as NotifyAvailableDatesWorkflowCommand } from './notify-available-dates';
-export { default as SelectGroupActionWorkflowCommand } from './select-group-action';
-export { default as SelectGroupWorkflowCommand } from './select-group';
-export { default as SelectAppointmentMembersWorkflowCommand } from './select-schedule-appointment-members';
+export { default as NavigateToGroupAppointmentWorkflowCommand } from './navigate-to-group-appointment';
 export { default as SignInWorkflowCommand } from './sign-in';
